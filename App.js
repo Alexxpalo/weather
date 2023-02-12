0083320d7678b3eb22e9b20fdac22be653b9e393
Alexxpalo <palosaaritobias@gmail.com>
2023-02-12 18:47:29 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Vibration } from 'react-native';
+
+import NumberBtn from './components/numberBtn';
+import Guesshistory from './components/Guesshistory';
 
 export default function App() {
+  const [guess, setGuess] = React.useState(0);
+  const [answer, SetAnswer] = React.useState(0);
+  const [score, SetScore] = React.useState(0);
+  const [history, Sethistory] = React.useState([]);
+
+
+  React.useEffect(() => {
+    SetAnswer(Math.floor(Math.random() * 10));
+  }, []);
+
+  async function guessNumber(guess) {
+
+    setGuess(guess);
+    Sethistory (history.concat(guess));
+
+    if (guess == answer) {
+
+      alert("You guessed it right!");
+
+      SetScore(score + 1);
+      SetAnswer(Math.floor(Math.random() * 9) + 1);
+    } 
+
+    else {
+      if (guess < answer) {
+        alert("You guessed it wrong, its higher!");
+      } else {
+        alert("You guessed it wrong, its lower!");
+      }
+      Vibration.vibrate(1000);
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text style={styles.scoreText}>Score: {score}</Text>
+
+      <Text style={styles.infoText} >Guess a number from 1 to 9</Text>
+      <View style={styles.Allbuttons}>
+      <NumberBtn name="1" onPress={guessNumber}/>
+      <NumberBtn name="2" onPress={guessNumber}/>
+      <NumberBtn name="3" onPress={guessNumber}/>
+      <NumberBtn name="4" onPress={guessNumber}/>
+      <NumberBtn name="5" onPress={guessNumber}/>
+      <NumberBtn name="6" onPress={guessNumber}/>
+      <NumberBtn name="7" onPress={guessNumber}/>
+      <NumberBtn name="8" onPress={guessNumber}/>
+      <NumberBtn name="9" onPress={guessNumber}/>
+      </View>
+      <View>
+        <Text style={styles.infoText}>Past guesses: </Text>
+        <Guesshistory list={history}/>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +70,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scoreText: {
+    color: 'white',
+    fontSize: 30,
+    marginBottom: 50
+  },
+  infoText: {
+    color: 'white',
+    fontSize: 15,
+    marginBottom: 5
+  },
+  Allbuttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
---
+++ components/Guesshistory.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Button, Text, StyleSheet } from 'react-native';
+
+class Guesshistory extends Component {
+
+    render() {
+        return (
+            <View>
+            <Text>test</Text>
+            </View>
+        );
+    }
+};
+
+
+export default Guesshistory;
